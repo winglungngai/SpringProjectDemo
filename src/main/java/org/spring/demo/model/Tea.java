2e44764aa3708d2cd29965c55960d2d93180a850
Wing Lung Ngai <w.l.ngai@student.tudelft.nl>
2013-07-13 23:32:40 +0200

    Add Restful Demo's

---
+++ src/main/java/org/spring/demo/model/Tea.java
@@ -0,0 +1,47 @@
+package org.spring.demo.model;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+ 
+@XmlRootElement(name = "tea")
+public class Tea {
+ 
+	String name;
+	int quanlity;
+	String origin;
+	
+	public String getOrigin() {
+		return origin;
+	}
+
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+
+	public String getName() {
+		return name;
+	}
+ 
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+ 
+	public int getQuanlity() {
+		return quanlity;
+	}
+ 
+	public void setQuanlity(int quanlity) {
+		this.quanlity = quanlity;
+	}
+ 
+	public Tea(String name, int quanlity, String origin) {
+		this.name = name;
+		this.quanlity = quanlity;
+		this.origin = origin;
+	}
+ 
+	public Tea() {
+	}
+ 
+}
